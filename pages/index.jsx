edcc320703fbc7277ec64b32e88edf8faa240d6d
MHCode <aaron20hc@gmail.com>
2022-12-25 23:55:40 -0500

    v1.1

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,7 @@
 import { Inter } from '@next/font/google';
-import CharacterList from '../src/components/character/CharacterList';
-import Layout from '../src/Layout/Layout';
+import CharacterList from './../src/components/character/CharacterList';
+import Layout from './../src/Layout/Layout';
 
-import CharacterProvider from '../src/providers/character.provider';
 import LocationProvider from '../src/providers/location.provider';
 import CurrentCharacter from '../src/components/character/CurrentCharacter';
 import { useContext } from 'react';
